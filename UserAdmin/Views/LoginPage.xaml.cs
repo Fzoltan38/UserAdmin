be8cdc1e914da06923bd95f0f9a2c7fad78e68d7
FZoltan <farkaszoltan28@gmail.com>
2026-09-11 17:51:22 +0200

    FindByEmail function is redy to use.

--- UserAdmin/Views/LoginPage.xaml.cs
+++ UserAdmin/Views/LoginPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserAdmin.Services;
 
 namespace UserAdmin.Views
 {
@@ -18,19 +19,52 @@ namespace UserAdmin.Views
     /// </summary>
     public partial class LoginPage : Page
     {
+        private readonly UserDbService _userDbService = new();
         public LoginPage()
         {
             InitializeComponent();
+            
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
+            var email = EmailBox.Text.Trim();
+            var password = PasswordBoxInput.Password;
 
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                ShowError("Érvénytelen email vagy jelszó.");
+            }
+
+            var user = _userDbService.FindByEmail(email);
+
+            if (user is null ) 
+            {
+                ShowError("Nem regisztrált tag.");
+                return;
+            }
+            else if (user.Email == email && user.Password == password)
+            {
+                MessageBox.Show("Regisztrált tag.");
+            }
+            else
+            {
+                ShowError("Nem megfelelő email vagy jelszó.");
+                return;
+            }
+
+            
         }
 
         private void RegisterLink_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new RegisterPage());
+        }
+
+        private void ShowError(string message)
+        {
+            ErrorText.Text = message;
+            ErrorText.Visibility = Visibility.Visible;
         }
     }
 }
