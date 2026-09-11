c75822b01cdddd06a5627967470ad5d43621fb4e
FZoltan <farkaszoltan28@gmail.com>
2026-09-11 17:10:25 +0200

    FindByEmail function is created.

--- UserAdmin/Services/UserDbService.cs
+++ UserAdmin/Services/UserDbService.cs
@@ -25,5 +25,42 @@ VALUES (@Username,@Email,@Password,@RegisteredAt)";
 
             connection.Close();
         }
+
+        public User FindByEmail(string email)
+        {
+            using var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+
+            string sql = @"SELECT `username`, `email`, `password`, `registeredAt` FROM `users` WHERE email = @email";
+
+            var cmd = new MySqlCommand(sql,connection);
+
+            cmd.Parameters.AddWithValue("@email",email);
+
+            var reader = cmd.ExecuteReader();
+
+            
+            if (reader.Read())
+            {
+                var user = new User
+                {
+                    Username = reader.GetString(0),
+                    Email = reader.GetString(1),
+                    Password = reader.GetString(2),
+                    RegisteredAt = reader.GetDateTime(3)
+                };
+
+                connection.Close();
+
+                return user;
+            }
+
+
+            connection.Close();
+
+            return null;
+           
+           
+        }
     }
 }
